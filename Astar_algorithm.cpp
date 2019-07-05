c64aea685e0bd51c5ea85ea6a2ec9402ce4e7cba
By <1023695613@qq.com>
2019-07-05 11:19:18 +0800

    normalize code

--- Astar_algorithm.cpp
+++ Astar_algorithm.cpp
@@ -1,199 +1,58 @@
 #include "include/Astar_algorithm.h"
 
-double DistenceToGoal(const Points& current, const Points& goal) {
-  //曼哈顿距离
-  return fabs(current.first - goal.first) + fabs(current.second - goal.second);
+//**进行一次总体的A*算法**
+//输入：文件序号
+//输出：无
+void SearchOneMap(int map_num_) {
+  //获得map信息
+  GrideInput map_info(map_num_);
+  map_info.GetOneGrid();
+  map_info.PrintMap();  //打印原始map
 
-  // //欧式距离平方
-  // return pow((current.first - goal.first), 2) +
-  //             pow((current.second - goal.second), 2);
+  //数据传入，构造类对象
+  Astar Astar_algorithm(map_info.get_grid_rows(), map_info.get_grid_columns(),
+                        map_info.get_start_pos(), map_info.get_goal_pos(),
+                        map_info.get_obstacle_pos());
 
-  // //欧式距离
-  // return sqrt(pow((current.first - goal.first), 2) +
-  //             pow((current.second - goal.second), 2));
-}
+  while (1) {
+    //规划路径
+    std::cout << "**********" << std::endl;
+    std::cout << "search num : " << Astar_algorithm.get_search_nums() + 1
+              << std::endl;
+    Astar_algorithm.AstarGetPath();  //以当前起点为起点进行一次路径规划
 
-std::vector<Points> Astar_Algorithm(
-    const int row_args, const int columns_args, const Points& start_args,
-    const Points& goal_args, const std::vector<Points>& close_list_args) {
-  std::priority_queue<CellInfo> open_list;
-  std::vector<Points> close_list(close_list_args);
-  std::map<Points, Points> save_path_hash;
-  std::vector<Points> path_result_list;  //存放结果
-
-  CellInfo start_info = {0, 0, start_args};  //初始化起点的信息
-  int search_successful_flg = 1, expand_points_count = 0;
-
-  open_list.push(start_info);  //起点入队列
-  while (!open_list.empty()) {
-    CellInfo current_cell_pos = open_list.top();
-    open_list.pop();
-    // std::cout << "(" << current_cell_pos.xoy_.first << " , "
-    //           << current_cell_pos.xoy_.second << ")"
-    //           << "  ";
-
-    if (current_cell_pos.xoy_ == goal_args) {  //找到终点，一次算法结束
-      search_successful_flg = 0;
+    if (Astar_algorithm.get_current_path().size() == 0) {
+      std::cout << "-——-——-——-——-——-——-——-———-——-——-——-" << std::endl;
+      std::cout << "|final result : no path to goal !!|" << std::endl;
+      std::cout << "-——-——-——-——-——-——-——-———-——-——-——-" << std::endl;
+      Astar_algorithm.PrintCountResult();
       break;
     }
 
-    if (std::find(close_list.begin(), close_list.end(),
-                  current_cell_pos.xoy_) == close_list.end()) {
-      close_list.push_back(current_cell_pos.xoy_);
-      ++expand_points_count;
-      // std::cout<<"("<<current_cell_pos.xoy_.first<<" ,
-      // "<<current_cell_pos.xoy_.second<<")"<<"  ";
+    while (!Astar_algorithm.get_current_path().empty()) {
+      //更新当前点各临近点的信息
+      Astar_algorithm.UpdataMapInfo();
 
-      std::vector<CellInfo> neighbors = GetNeighbors(
-          row_args, columns_args, start_args, goal_args, current_cell_pos.xoy_);
-
-      for (int i = 0; i < neighbors.size(); ++i) {
-        if (std::find(close_list.begin(), close_list.end(),
-                      neighbors[i].xoy_) == close_list.end()) {
-          // g(n)
-          neighbors[i].cost_to_start_ += current_cell_pos.cost_to_start_ + 1;
-          // f(n)=g(n)+h(n)
-          neighbors[i].all_cost_ = neighbors[i].cost_to_start_ +
-                                   DistenceToGoal(neighbors[i].xoy_, goal_args);
-          open_list.push(neighbors[i]);
-          save_path_hash[neighbors[i].xoy_] = current_cell_pos.xoy_;
-        }
+      if (Astar_algorithm.NextStepIsInObstacleList()) {
+        break;  //当前点要移动到的下一个点是obstacle
+      } else {
+        Astar_algorithm.StartMove();
       }
     }
-  }
-  //   std::cout << std::endl << std::endl;
-  //   std::cout << "----------" << close_list.size() << "----------" <<
-  //   std::endl; for (int i = 0; i < close_list.size(); ++i) {
-  //     std::cout << "(" << close_list[i].first << " , " <<
-  //     close_list[i].second
-  //               << ")"
-  //               << "  ";
-  //   }
-  //   std::cout << std::endl << std::endl;
 
-  // there is one shortest path to goal
-  if (search_successful_flg) {
-    std::cout << "search fail !!" << std::endl;
-  }
-  // nope path to goal
-  else {
-    std::cout << "search successfully !!" << std::endl;
-    Points node = goal_args;
-    //得到最短路径的坐标向量
-    while (node != start_info.xoy_) {
-      path_result_list.push_back(node);
-      node = save_path_hash[node];
-      // std::cout<<"("<<node.first<<" , "<<node.second<<")"<<std::endl;
+    if (Astar_algorithm.ArriveGoal()) {  //走到了终点
+      std::cout << "-——-——-——-——-——-——-——-———-——-——-——-——-" << std::endl;
+      std::cout << "|final result: get goal successflly!!|" << std::endl;
+      std::cout << "-——-——-——-——-——-——-——-———-——-——-——-——-" << std::endl;
+      Astar_algorithm.PrintCountResult();
+      break;
     }
   }
-
-  // map output
-  for (int i = 0; i < row_args; ++i) {
-    for (int j = 0; j < columns_args; ++j) {
-      if (start_args.first == i && start_args.second == j)
-        std::cout << "s ";
-      else if (goal_args.first == i && goal_args.second == j)
-        std::cout << "g ";
-      else if (std::find(close_list_args.begin(), close_list_args.end(),
-                         Points(i, j)) != close_list_args.end())
-        std::cout << "x ";
-      else if (std::find(path_result_list.begin(), path_result_list.end(),
-                         Points(i, j)) != path_result_list.end())
-        std::cout << "o ";
-      else
-        std::cout << "_ ";
-    }
-    std::cout << std::endl;
-  }
-  std::cout << "shortest path step nums : " << path_result_list.size()
-            << "    expand point nums : " << expand_points_count << std::endl;
-
-  std::cout << std::endl << std::endl;
-
-  // for (int i = 0; i < path_result_list.size(); i++) {
-  //   std::cout << "(" << path_result_list[i].first << ","
-  //             << path_result_list[i].second << ")" << std::endl;
-  // }
-  return path_result_list;
-
-  // return path_result_list;
 }
 
-// 找出当前点的neighbors
-// 对neighbors进行初始化时，根据起点与终点的方向，在neighbors的cost_to_start_参数进行方向性的初始化，
-// 及添加起点至终点向量的0.01倍作为其初值，使其轨迹方向性更好
-std::vector<CellInfo> GetNeighbors(const int row_args, const int columns_args,
-                                   const Points& start_args,
-                                   const Points& goal_args,
-                                   const Points& current_pos) {
-  std::vector<CellInfo> neighbors;
-  // Up
-  if ((current_pos.first - 1) >= 0) {
-    neighbors.push_back({0, (goal_args.first - start_args.first) * 0.01,
-                         Points(current_pos.first - 1, current_pos.second)});
-  }
-  // Down
-  if ((current_pos.first + 1) < row_args) {
-    neighbors.push_back({0, -(goal_args.first - start_args.first) * 0.01,
-                         Points(current_pos.first + 1, current_pos.second)});
-  }
-  // Left
-  if ((current_pos.second - 1) >= 0) {
-    neighbors.push_back({0, -(goal_args.second - start_args.second) * 0.01,
-                         Points(current_pos.first, current_pos.second - 1)});
-  }
-  // Right
-  if ((current_pos.second + 1) < columns_args) {
-    neighbors.push_back({0, (goal_args.second - start_args.second) * 0.01,
-                         Points(current_pos.first, current_pos.second + 1)});
-  }
-
-  return neighbors;
-}
-
-//获得当前节点的neighbors，但仅仅包含障碍信息
-void UpdataMapInfo(const int row_args, const int columns_args,
-                   const Points& start_args,
-                   const std::vector<Points>& map_obstacle_list_,
-                   std::vector<Points>& current_obstacle_list_) {
-  if ((start_args.first - 1) >= 0) {
-    if (IsInObstacleList(Points(start_args.first - 1, start_args.second),
-                         map_obstacle_list_))
-      current_obstacle_list_.push_back(
-          Points(start_args.first - 1, start_args.second));
-  }
-  // Down
-  if ((start_args.first + 1) < row_args) {
-    if (IsInObstacleList(Points(start_args.first + 1, start_args.second),
-                         map_obstacle_list_))
-      current_obstacle_list_.push_back(
-          Points(start_args.first + 1, start_args.second));
-  }
-  // Left
-  if ((start_args.second - 1) >= 0) {
-    if (IsInObstacleList(Points(start_args.first, start_args.second - 1),
-                         map_obstacle_list_))
-      current_obstacle_list_.push_back(
-          Points(start_args.first, start_args.second - 1));
-  }
-  // Right
-  if ((start_args.second + 1) < columns_args) {
-    if (IsInObstacleList(Points(start_args.first, start_args.second + 1),
-                         map_obstacle_list_))
-      current_obstacle_list_.push_back(
-          Points(start_args.first, start_args.second + 1));
-  }
-}
-
-//判断当前点是否在障碍list中
-// return 1:on  0:nop
-bool IsInObstacleList(const Points& point,
-                      const std::vector<Points>& obstacle_list) {
-  return std::find(obstacle_list.begin(), obstacle_list.end(), point) !=
-         obstacle_list.end();
-}
-
-//*********************************************************************
+//**A*算法构造函数**
+//输入：地图的行数、列数、起点、终点以、所有的障碍物点
+//输出：无
 Astar::Astar(int row, int column, Points statr, Points goal,
              std::vector<Points> obstacle_list_)
     : row_(row),
@@ -202,9 +61,13 @@ Astar::Astar(int row, int column, Points statr, Points goal,
       goal_pos_(goal),
       map_obstacle_list_(obstacle_list_) {
   current_start_ = start_pos_;
+  all_expand_points_count_ = 0;
+  search_nums_count_ = 0;
 }
 
-//获得当前节点的neighbors,一次A*算法中
+//**在一次A*算法中获得当前点的四个临近点**
+//输入：当前点的坐标信息
+//输出：四个临近点的信息
 std::vector<CellInfo> Astar::GetNeighbors(const Points& current_pos) {
   std::vector<CellInfo> neighbors;
   // Up
@@ -230,42 +93,43 @@ std::vector<CellInfo> Astar::GetNeighbors(const Points& current_pos) {
   return neighbors;
 }
 
-//获得当前节点的neighbors，但仅仅包含障碍信息,整体A*算法中
+//**在整体A*算法中获得当前起点的四个临近点的信息**
+//输入：无
+//输出：无
 void Astar::UpdataMapInfo() {
+  // UP
   if ((current_start_.first - 1) >= 0) {
-    if (IsInObstacleList(
-            Points(current_start_.first - 1, current_start_.second),
-            map_obstacle_list_))
+    if (IsInList(Points(current_start_.first - 1, current_start_.second),
+                 map_obstacle_list_))
       current_obstacle_list_.push_back(
           Points(current_start_.first - 1, current_start_.second));
   }
   // Down
   if ((current_start_.first + 1) < row_) {
-    if (IsInObstacleList(
-            Points(current_start_.first + 1, current_start_.second),
-            map_obstacle_list_))
+    if (IsInList(Points(current_start_.first + 1, current_start_.second),
+                 map_obstacle_list_))
       current_obstacle_list_.push_back(
           Points(current_start_.first + 1, current_start_.second));
   }
   // Left
   if ((current_start_.second - 1) >= 0) {
-    if (IsInObstacleList(
-            Points(current_start_.first, current_start_.second - 1),
-            map_obstacle_list_))
+    if (IsInList(Points(current_start_.first, current_start_.second - 1),
+                 map_obstacle_list_))
       current_obstacle_list_.push_back(
           Points(current_start_.first, current_start_.second - 1));
   }
   // Right
   if ((current_start_.second + 1) < column_) {
-    if (IsInObstacleList(
-            Points(current_start_.first, current_start_.second + 1),
-            map_obstacle_list_))
+    if (IsInList(Points(current_start_.first, current_start_.second + 1),
+                 map_obstacle_list_))
       current_obstacle_list_.push_back(
           Points(current_start_.first, current_start_.second + 1));
   }
 }
 
-//一次A*算法，以当前起点为起点
+//**以当前点为起点进行一次A*算法的计算**
+//输入：无
+//输出：无
 void Astar::AstarGetPath() {
   {
     std::priority_queue<CellInfo> open_list;  //存放将要遍历的点
@@ -275,7 +139,8 @@ void Astar::AstarGetPath() {
     std::vector<Points> path_result_list;     //存放结果
 
     CellInfo start_info = {0, 0, current_start_};  //初始化起点的信息
-    int search_successful_flg = 1, expand_points_count = 0;
+    int search_successful_flg = 1;                 //搜索成功标志
+    current_expand_points_count_ = 0;
 
     open_list.push(start_info);  //起点入队列
 
@@ -291,9 +156,7 @@ void Astar::AstarGetPath() {
       //如果不在closelist中，可以expand
       if (!IsInList(current_cell_pos.xoy_, close_list)) {
         close_list.push_back(current_cell_pos.xoy_);
-        ++expand_points_count;
-        // std::cout<<"("<<current_cell_pos.xoy_.first<<" ,
-        // "<<current_cell_pos.xoy_.second<<")"<<"  ";
+        ++current_expand_points_count_;
 
         std::vector<CellInfo> neighbors = GetNeighbors(current_cell_pos.xoy_);
 
@@ -310,61 +173,68 @@ void Astar::AstarGetPath() {
         }
       }
     }
-    //   std::cout << std::endl << std::endl;
-    //   std::cout << "----------" << close_list.size() << "----------" <<
-    //   std::endl; for (int i = 0; i < close_list.size(); ++i) {
-    //     std::cout << "(" << close_list[i].first << " , " <<
-    //     close_list[i].second
-    //               << ")"
-    //               << "  ";
-    //   }
-    //   std::cout << std::endl << std::endl;
 
-    // there is one shortest path to goal
+    // nope path to goal
     if (search_successful_flg) {
       std::cout << "search fail !!" << std::endl;
     }
-    // nope path to goal
+    // there is one shortest path to goal
     else {
       std::cout << "search successfully !!" << std::endl;
       Points node = goal_pos_;
-      //得到最短路径的坐标向量
+      //得到最短路径的坐标list
       while (node != start_info.xoy_) {
         path_result_list.push_back(node);
         node = save_path_hash[node];
-        // std::cout<<"("<<node.first<<" , "<<node.second<<")"<<std::endl;
       }
     }
 
-    // map output
-    for (int i = 0; i < row_; ++i) {
-      for (int j = 0; j < column_; ++j) {
-        if (current_start_.first == i && current_start_.second == j)
-          std::cout << "s ";
+    current_path_.clear();             //当前路径清零
+    current_path_ = path_result_list;  //赋值当前路径
+    all_expand_points_count_ += current_expand_points_count_;  // expand计数累加
 
-        else if (goal_pos_.first == i && goal_pos_.second == j)
-          std::cout << "g ";
+    SearchResultPrint();  //打印一次搜索结果
 
-        else if (IsInList(Points(i, j), current_obstacle_list_))
-          std::cout << "x ";
-
-        else if (IsInList(Points(i, j), path_result_list))
-          std::cout << "o ";
-        else
-          std::cout << "_ ";
-      }
-      std::cout << std::endl;
-    }
-    std::cout << "shortest path step nums : " << path_result_list.size()
-              << "    expand point nums : " << expand_points_count << std::endl;
-
-    std::cout << std::endl << std::endl;
-
-    // for (int i = 0; i < path_result_list.size(); i++) {
-    //   std::cout << "(" << path_result_list[i].first << ","
-    //             << path_result_list[i].second << ")" << std::endl;
-    // }
-    current_path_.clear();
-    current_path_ = path_result_list;
+    ++search_nums_count_;  //搜索次数自增1
   }
+}
+
+//**打印一次搜索结果**
+//输入：无
+//输出：无
+void Astar::SearchResultPrint() {
+  for (int i = 0; i < row_; ++i) {
+    for (int j = 0; j < column_; ++j) {
+      if (current_start_.first == i && current_start_.second == j)
+        std::cout << "s ";
+
+      else if (goal_pos_.first == i && goal_pos_.second == j)
+        std::cout << "g ";
+
+      else if (IsInList(Points(i, j), current_obstacle_list_))
+        std::cout << "x ";
+
+      else if (IsInList(Points(i, j), current_path_))
+        std::cout << "o ";
+      else
+        std::cout << "_ ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << "shortest path step nums : " << current_path_.size()
+            << "    expand point nums : " << current_expand_points_count_
+            << std::endl;
+
+  std::cout << std::endl << std::endl;
+}
+
+//**打印计数结果**
+//输入：无
+//输出：无
+void Astar::PrintCountResult() {
+  std::cout << std::endl
+            << "The nums of search : " << search_nums_count_
+            << "  total expanded nums : " << all_expand_points_count_
+            << std::endl
+            << std::endl;
 }
--- include/Astar_algorithm.h
+++ include/Astar_algorithm.h
@@ -12,9 +12,9 @@
 
 //每一个cell的信息，使用结构体定义
 struct CellInfo {
-  double all_cost_;
-  double cost_to_start_;
-  Points xoy_;
+  double all_cost_;       //总花费
+  double cost_to_start_;  // g-value
+  Points xoy_;            //坐标点
   bool operator<(const CellInfo& pos) const {
     // if f-vaule 相同，比较 g-vaule
     if (all_cost_ == pos.all_cost_)
@@ -24,68 +24,17 @@ struct CellInfo {
   }
 };
 
-//获得当前节点的neighbors
-std::vector<CellInfo> GetNeighbors(const int row_args, const int columns_args,
-                                   const Points& start_args,
-                                   const Points& goal_args,
-                                   const Points& current_pos);
+void SearchOneMap(int map_num_);  //对一张地图进行算法搜索
 
-//获得当前节点的neighbors，但仅仅包含障碍信息
-void UpdataMapInfo(const int row_args, const int columns_args,
-                   const Points& start_args,
-                   const std::vector<Points>& map_obstacle_list,
-                   std::vector<Points>& close_list);
-//判断当前点是否在障碍list中
-bool IsInObstacleList(const Points& point,
-                      const std::vector<Points>& obstacle_list);
-
-// A*算法函数
-std::vector<Points> Astar_Algorithm(const int row_, const int columns_,
-                                    const Points& statr_, const Points& goal_,
-                                    const std::vector<Points>& close_list_);
-//计算当前点与终点距离的函数
-double DistenceToGoal(const Points& current, const Points& goal);
-
-//****************************************************
 class Astar {
  public:
   Astar(int row_, int columns_, Points statr_, Points goal_,
-        std::vector<Points> obstacle_list_);
-  Astar(const Astar& as) = delete;
+        std::vector<Points> obstacle_list_);  //构造函数
+  Astar(const Astar& as) = delete;            //不使用复制构造函数
 
-  // A*算法函数
-  void AstarGetPath();
-
-  //类内私有成员赋值函数
-  inline int& set_row() { return row_; }
-  inline int& set_column() { return column_; }
-  inline Points& set_start_pos() { return start_pos_; }
-  inline Points& set_goal_pos() { return goal_pos_; }
-  inline Points& set_current_start() { return current_start_; }
-  inline std::vector<Points>& set_map_obstacle_list() {
-    return map_obstacle_list_;
-  }
-  inline std::vector<Points>& set_current_obstacle_list() {
-    return current_obstacle_list_;
-  }
-  inline std::vector<Points>& set_current_path() { return current_path_; }
-  //******************
-
-  //获取类内私有成员函数
-  inline int get_row() const { return row_; }
-  inline int get_column() const { return column_; }
-  inline Points get_start_pos() const { return start_pos_; }
-  inline Points get_goal_pos() const { return goal_pos_; }
-  inline Points get_current_start() const { return current_start_; }
-  inline std::vector<Points> get_map_obstacle_list() const {
-    return map_obstacle_list_;
-  }
-  inline std::vector<Points> get_current_obstacle_list() const {
-    return current_obstacle_list_;
-  }
-  inline std::vector<Points> get_current_path() const { return current_path_; }
-  //******************
-
+  //******下面函数用于一次A*规划算法中*******//
+  void AstarGetPath();       // 一次A*算法函数
+  void SearchResultPrint();  //打印搜索结果
   //计算当前点与终点距离的函数
   inline double DistenceToGoal(const Points& current) {
     //曼哈顿距离
@@ -103,27 +52,71 @@ class Astar {
 
   //获得当前节点的neighbors
   std::vector<CellInfo> GetNeighbors(const Points& current_pos);
+  //**************************************//
 
-  //获得当前节点的neighbors，但仅仅包含障碍信息
-  void UpdataMapInfo();
+  //**********下面函数用于整体算法中**********//
+  void UpdataMapInfo();  //获得当前节点的neighbors，但仅仅包含障碍信息
 
+  // move one step along the shortest path
+  //沿着当前最短路径走一步
   void StartMove() {
     current_start_ = current_path_.back();
     current_path_.pop_back();
   }
 
+  //判断是否走到了终点
   bool ArriveGoal() { return current_start_ == goal_pos_; }
+
+  //判断要移动的下一个点是否为障碍物
   inline bool NextStepIsInObstacleList() {
     return IsInList(current_path_.back(), current_obstacle_list_);
   }
+  //打印计数结果
+  void PrintCountResult();
+  //**************************************//
+
+  //**********获取类内私有成员函数**********//
+  inline int get_row() const { return row_; }
+  inline int get_column() const { return column_; }
+  inline Points get_start_pos() const { return start_pos_; }
+  inline Points get_goal_pos() const { return goal_pos_; }
+  inline Points get_current_start() const { return current_start_; }
+  inline std::vector<Points> get_map_obstacle_list() const {
+    return map_obstacle_list_;
+  }
+  inline std::vector<Points> get_current_obstacle_list() const {
+    return current_obstacle_list_;
+  }
+  inline std::vector<Points> get_current_path() const { return current_path_; }
+  inline int get_all_expand_nums() const { return all_expand_points_count_; }
+  inline int get_search_nums() const { return search_nums_count_; }
+  //************************************//
+
+  //**********类内私有成员赋值函数**********//
+  inline int& set_row() { return row_; }
+  inline int& set_column() { return column_; }
+  inline Points& set_start_pos() { return start_pos_; }
+  inline Points& set_goal_pos() { return goal_pos_; }
+  inline Points& set_current_start() { return current_start_; }
+  inline std::vector<Points>& set_map_obstacle_list() {
+    return map_obstacle_list_;
+  }
+  inline std::vector<Points>& set_current_obstacle_list() {
+    return current_obstacle_list_;
+  }
+  inline std::vector<Points>& set_current_path() { return current_path_; }
+  //************************************//
 
  private:
-  int row_, column_;
-  Points start_pos_, goal_pos_;
-  Points current_start_;                   //当前起点
-  std::vector<Points> map_obstacle_list_;  //所有障碍物list
-  std::vector<Points> current_path_;       //当前路径
-  std::vector<Points> current_obstacle_list_;
+  int row_, column_;                           // map的行、列数
+  Points start_pos_, goal_pos_;                // map的起点与终点坐标
+  Points current_start_;                       //当前起点
+  std::vector<Points> map_obstacle_list_;      //所有障碍物list
+  std::vector<Points> current_path_;           //当前路径
+  std::vector<Points> current_obstacle_list_;  //当前已知的障碍物list
+  int current_expand_points_count_,            //一次算法中的expand计数
+      all_expand_points_count_,                //整体算法中的expand计数
+      search_nums_count_;                      //搜索次数计数
 
   //判断点是否在list中
   inline bool IsInList(const Points& point, const std::vector<Points>& list) {
